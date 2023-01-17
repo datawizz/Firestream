0627816bb07d551056e7fbed22cd4fda55b37d37
datawizz <36938650+datawizz@users.noreply.github.com>
2023-01-17 09:27:02 -0800

    organize microservices

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
+### BOOTSTRAP ###
 
 # This script bootstraps the project
-
 
 # 1. Setup a Kind cluster
 # 2. Setup networking proxy into Kind DNS (exposes internal services to the devcotainer, used in testing and development)
@@ -10,34 +12,57 @@
 # 6. Build a Spark Image with the Jar included
 # 5. Print to console information to connect
 
-#!/bin/bash
+###############################################################################
+### 1. Ensure Docker is available                                           ###
+###############################################################################
 
-# Attempt to ping google.com
-ping -c 1 -W 2 google.com &> /dev/null
-if [ $? -ne 0 ]; then
-  # Check if Kind is accessible
-  if ! kind get clusters; then
-    # If not, run delete kind
-    kind delete cluster
-  fi
+# Assert that each command finishes without error
+# set -e
+
+# Start Docker
+echo "Starting Docker"
+# bash /usr/local/share/docker-init.sh
+#Ensure it is started
+docker --version
+
+###############################################################################
+### 2. KinD (Kubernetes in Docker) Cluster                                  ###
+###############################################################################
+
+# The project's KinD cluster is used for DNS resolution and needs to be available
+
+# Define the cluster's name
+cluster_name="fireworks"
+
+# Check if the project's kind cluster is running
+existing_cluster=$(kind get clusters | grep "$cluster_name")
+
+if [ -z "$existing_cluster" ]; then
+  echo "the cluster is not running, create it"
+  # # Create a KinD cluster on the host's Docker Engine
+  kind create cluster --config=/workspace/k8/kind/values.yaml --name $cluster_name
+else
+  echo "the cluster is running, attach to it"
+  # TODO don't delete the cluster and recreate it, attach to it by exporting kubeconfig and importing to kubectl
+  kind delete cluster --name $cluster_name
+  kind create cluster --config=/workspace/k8/kind/values.yaml --name $cluster_name
 fi
 
+# Wait a moment for the cluster to come up
+echo "Sleeping for 20 seconds to let KinD initialize"
+sleep 20
 
 ###############################################################################
-### 1. Kind K8 Cluster                                                      ###
+### 3. Resolve DNS through Kubernetes Control Plane                         ###
 ###############################################################################
-
-# Create a KinD cluster on the host's Docker Engine
-kind create cluster --config=/workspace/charts/1_kind/values.yaml
-
-###############################################################################
-### 2. Resolve DNS through Kubernetes Control Plane                         ###
-###############################################################################
-
 
 # Find the IP of the Kind Control Plane Node (dynamically assigned with each cluster restart)
-export KIND_IP=$(docker container inspect kind-control-plane --format '{{ .NetworkSettings.Networks.kind.IPAddress }}')
+export KIND_IP=$(docker container inspect fireworks-control-plane --format '{{ .NetworkSettings.Networks.kind.IPAddress }}')
 echo $KIND_IP
+
+
+# TODO this needs to be idempotent as the second run after a devcontainer rebuild has conflicts with the existing config
+
 # Set the route for Services inside the cluster
 # This is also used for the CoreDNS service
 sudo ip route add 10.96.0.0/16 via $KIND_IP
@@ -50,6 +75,8 @@ sudo ip route add 10.244.0.0/16 via $KIND_IP
 
 # sudo ip route add 10.100.0.0/24 via $KIND_IP
 
+# TODO try to ping outside resources
+# ping Google.com
 
 ###############################################################################
 ### 3. Local Docker Registry                                                ###
@@ -58,14 +85,15 @@ sudo ip route add 10.244.0.0/16 via $KIND_IP
 # export DOCKER_PORT=5000
 # export ARTIFACT_REPO=localhost:5000
 
+# TODO make this idempotent and clear the registry from previous runs?
+
 # Setup Docker Registry
 printf "\nInstalling Local Docker Registry...\n"
 docker pull registry
 
 printf "\nStarting Local Docker Registry...\n\n"
 
-
-reg_name='kind-registry'
+reg_name='fireworks-registry'
 reg_port='5000'
 running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
 if [ "${running}" != 'true' ]; then
@@ -75,47 +103,58 @@ if [ "${running}" != 'true' ]; then
 fi
 
 # Connect registry running in Docker to the Kind network used in the cluster
-docker network connect "kind" "kind-registry"
+docker network connect "kind" "${reg_name}"
 
 
-# Build each of the project's Dockerfiles and push the images to the local registry
+###############################################################################
+### Build Docker Images                                                     ###
+###############################################################################
 
-# Build the Spark Driver/Executor
+# export DOCKER_HOST=tcp://127.0.0.1:2375
 
+# Build the dependencies and make it available on the local Docker registry
+docker build --target dependencies -f /workspace/Dockerfile -t localhost:5000/dependencies:latest .
+docker push localhost:5000/dependencies:latest
 
-# # Build the dashboard
-# docker build -f /workspaces/k8-spark-kafka/charts/6_dashboard/Dockerfile ./charts/6_dashboard
-
-
-# ###############################################################################
-# ### Build Images                                                            ###
-# ###############################################################################
-
-# cd 
-
-# /workspace/scala/transform/target/scala-2.13/kafka-streams-stateful-processing-assembly-1.0.jar
+# Build the Spark SQL Structured Streaming Stateful container
+docker build -f /workspace/services/scala/spark_structured_streaming_stateful/Dockerfile -t localhost:5000/spark_structured_streaming_stateful:latest .
 
 
 
+###############################################################################
+### Build Artifacts                                                         ###
+###############################################################################
 
-# ###############################################################################
-# ### Build Images                                                            ###
-# ###############################################################################
+# # # cd 
 
-# Build the images which will be used in the Spark Cluster
+# # # /workspace/scala/transform/target/scala-2.13/kafka-streams-stateful-processing-assembly-1.0.jar
 
 
 
 
-# ###############################################################################
-# ### K8 Services                                                             ###
-# ###############################################################################
+
+
+
+# # ###############################################################################
+# # ### K8 Services                                                             ###
+# # ###############################################################################
 
 # Install Bitnami repo
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 # Install Kafka using local Values.yaml overrides
-helm install kafka bitnami/kafka -f /workspace/charts/2_kafka/values.yaml
+helm install kafka bitnami/kafka -f /workspace/k8/kafka/values.yaml
 
 # Install Spark using local Values.yaml overrides
-helm install spark bitnami/spark -f /workspace/charts/3_spark/values.yaml
+helm install spark bitnami/spark -f /workspace/k8/spark/values.yaml
+
+
+
+
+# # ###############################################################################
+# # ### Sleep                                                                   ###
+# # ###############################################################################
+
+# # Keep the container alive indefinitely
+# Only needed if this is the entrypoint script of the project
+# sleep infinity
