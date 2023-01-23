ceac4937de2e6d88b97fbbf53bc0601c4ad68c66
datawizz <36938650+datawizz@users.noreply.github.com>
2023-01-23 02:30:23 +0000

    [FIX] Graceful disconnections

--- services/javascript/websocket_middleware/src/server.js
+++ services/javascript/websocket_middleware/src/server.js
@@ -1,18 +1,15 @@
-
-
-// BOOTSTRAP_SERVERS = "kafka.default.svc.cluster.local:9092"
-// kafka-zookeeper.default.svc.cluster.local:2181
-
-
+// A Middleware to receive messages from Kafka and send to websocket clients
 
 const { Kafka } = require('kafkajs')
 const WebSocket = require('ws');
+
+const debug = false
 
 const kafka = new Kafka({
     clientId: 'websocket_middleware',
     brokers: ['kafka.default.svc.cluster.local:9092']
 })
-const consumer = kafka.consumer({ groupId: 'my-group3' })
+const consumer = kafka.consumer({ groupId: 'WebSocketMiddleware' })
 
 // const topics = ['metronome', 'spark_structured_streaming_stateful']
 // const topics = ['pyspark_wiener_process_stateless']
@@ -47,8 +44,13 @@ const consumeMessages = async () => {
                 value
             }
             topicArrays[topic].push(decoded_message);
+            // Verbose logging
+            if (debug) {
+                console.log(topic);
+                console.log(topicArrays[topic].length);
+            }
             if (topicArrays[topic].length > 100) {
-                topicArrays[topic].shift();
+                topicArrays[topic].pop();
             }
         },
     })
@@ -61,56 +63,74 @@ consumeMessages();
 // websocket server setup
 const wss = new WebSocket.Server({ port: 8080 });
 const clients = [];
-// wss.on('connection', (ws) => {
-//     // add the new client to the list of connected clients
-//     clients.push(ws);
 
-//     // send the current message queue to the new websocket client
-//     topics.forEach(topic => {
-//         ws.send(JSON.stringify(topicArrays[topic]));
-//     });
-
-//     // send new messages to all connected websocket clients as they come in
-//     setInterval(() => {
-//         topics.forEach(topic => {
-//             while (topicArrays[topic].length > 0) {
-//                 clients.forEach(client => {
-//                     client.send(JSON.stringify(topicArrays[topic].shift()));
-//                 });
-//             }
-//         });
-//     }, 100);
-
-//     // remove the client from the list of connected clients when the connection is closed
-//     ws.on('close', () => {
-//         const index = clients.indexOf(ws);
-//         if (index !== -1) {
-//             clients.splice(index, 1);
-//         }
-//     });
-// });
 
 wss.on('connection', (ws) => {
     // add the new client to the list of connected clients
-    clients.push({ ws, connected: true });
-    // send the current message queue to the new websocket client
-    topics.forEach(topic => {
-        ws.send(JSON.stringify(topicArrays[topic]));
-    });
+    clients.push({ ws, connected: true, lastPong: new Date() });
+
+    // Send data to all connected clients from queue
     setInterval(() => {
         topics.forEach(topic => {
             while (topicArrays[topic].length > 0) {
+                // Remove the first element of the queue
+                const _message = topicArrays[topic].shift();
+                // Send the first element to all connected clients
                 clients.forEach(client => {
-                    if (client.connected) client.ws.send(JSON.stringify(topicArrays[topic].shift()));
+                    // console.log(JSON.stringify(topicArrays[topic][0]).toString());
+                    if (client.connected) client.ws.send(JSON.stringify(_message).toString());
                 });
+
             }
         });
     }, 100);
-    // remove the client from the list of connected clients when the connection is closed
+
+    // send a ping message every 5 seconds
+    setInterval(() => {
+        clients.forEach(client => {
+            if (client.connected) {
+                client.ws.ping();
+                if (debug) {
+                    console.log('Ping sent to client: ', clients.find(client => client.ws === ws));
+                }
+            }
+        });
+    }, 5000);
+
+
+    // listen for pong responses from the client and update state to lastPong instant
+    ws.on('pong', () => {
+        clients.forEach(client => {
+            if (client.ws === ws) {
+                client.lastPong = new Date();
+            }
+        });
+    });
+
+    // check every 10 seconds if client is still connected
+    setInterval(() => {
+        clients.forEach(client => {
+            if (client.ws === ws && (new Date() - client.lastPong) > 10000) {
+                client.connected = false;
+                console.log('Client disconnected due to inactivity');
+            }
+        });
+    }, 10000);
+
+
+
+    // remove the client from the list of connected clients when the connection is closed or there is an error
     ws.on('close', () => {
         const index = clients.indexOf(ws);
         if (index !== -1) {
             clients[index].connected = false;
         }
     });
+    ws.on('error', () => {
+        const index = clients.indexOf(ws);
+        if (index !== -1) {
+            clients[index].connected = false;
+        }
+    });
+
 });
--- services/javascript/websocket_middleware/test/client.js
+++ services/javascript/websocket_middleware/test/client.js
@@ -1,5 +1,9 @@
 const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:8080');
+
+// const _address = 'ws://localhost:8080'
+const _address = 'ws://websocket-middleware.default.svc.cluster.local:8080'
+
+const ws = new WebSocket(_address);
 
 ws.on('open', function open() {
     console.log('connected');
@@ -8,12 +12,25 @@ ws.on('open', function open() {
 ws.on('message', function incoming(data) {
     try {
         const jsonData = JSON.parse(data);
-        // const jsonPayload = JSON.parse(jsonData.value)
-        // if (jsonData.topic != 'metronome') {
-        //     console.log(jsonData);
-        // }
         console.log(jsonData)
     } catch (e) {
         console.log('Error:', e);
     }
 });
+
+// Respond to liveliness checks
+ws.on('ping', () => {
+    console.log("Received ping from server, sending pong...");
+    ws.pong();
+});
+
+ws.on('error', function (err) {
+    console.log('Error:', err);
+    // same code as in 'close' event listener
+});
+
+ws.on('close', function () {
+    console.log('disconnected');
+    // same code as in 'error' event listener
+});
+
