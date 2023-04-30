6f96dcd0078427c910c42b81d6099799601daf81
datawizz <36938650+datawizz@users.noreply.github.com>
2023-04-30 20:03:45 +0000

    [FIX] Docker host multi arch

--- bin/host_scripts/host-docker.sh
+++ bin/host_scripts/host-docker.sh
@@ -1,34 +1,37 @@
-#!/usr/bin/env bash
-
 # Install docker on Debian
-# This script should be run directly on a clean linux install
+# This script should be run directly on a clean Ubuntu host to install the required VM engine.
 
 set -e
 
-export DEBIAN_FRONTEND=noninteractive 
+#TODO check that this is run as root
 
-apt-get update && \
-apt-get -y install \
+export DEBIAN_FRONTEND=noninteractive && apt update -y && apt -y install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release \
-    acl
+    lsb-release
 
 
 #TODO make this idempotent
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install Docker
-export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+export DEBIAN_FRONTEND=noninteractive && sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-# Add user to docker permissions
-usermod -a -G docker $USER
-setfacl --modify user:$USER:rw /var/run/docker.sock
+# Create docker group and add current user
+sudo groupadd docker
+sudo usermod -a -aG docker $USER
+
+# Modify permissions on docker socket
+sudo chown :docker /var/run/docker.sock
+sudo chmod 660 /var/run/docker.sock
+
+# Restart Docker service
+systemctl restart docker
 
 # Establish services
 systemctl enable docker.service
@@ -41,4 +44,7 @@ update-rc.d docker defaults
 
 
 # Test if docker is configured correctly
-docker run hello-world
+# docker run hello-world
+
+
+echo "Docker Installtation Complete. The system needs to be rebooted for changes to take effect.
