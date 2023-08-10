e239cf690f43329dd5c3ae06730a247ae4341c19
Datawizz <36938650+datawizz@users.noreply.github.com>
2023-08-10 03:23:43 +0000

    [ADD] Websocket Middleware

---
+++ src/api/websocket-middleware/bootstrap.sh
@@ -0,0 +1,39 @@
+
+
+
+PLUGIN_DIR="/workspace/src/api/"
+
+### Variables ###
+
+SERVICE_NAME="websocket-middleware"
+CONTAINER_REGISTRY_PREFIX="k3d-fireworks."
+CONTAINER_REGISTRY_URL="localhost"
+CONTAINER_REGISTRY_URL_FULL=$CONTAINER_REGISTRY_PREFIX$CONTAINER_REGISTRY_URL
+
+cd $PLUGIN_DIR$SERVICE_NAME
+
+
+### Build ###
+
+IMAGE_TAG=$CONTAINER_REGISTRY_URL_FULL:$CONTAINER_REGISTRY_PORT/$SERVICE_NAME:$GIT_COMMIT_HASH
+
+docker build \
+  --build-arg SERVICE_NAME=$SERVICE_NAME \
+  -t $IMAGE_TAG \
+  .
+
+docker push $IMAGE_TAG
+
+
+cd $PLUGIN_DIR$SERVICE_NAME/chart/websocket-middleware
+
+
+
+helm upgrade --install $SERVICE_NAME . \
+    --set image.repository=$CONTAINER_REGISTRY_URL_FULL:$CONTAINER_REGISTRY_PORT/$SERVICE_NAME \
+    --set image.tag=$GIT_COMMIT_HASH \
+    --set image.pullPolicy=Always \
+    -f /workspace/src/api/websocket-middleware/manifest.a.yaml
+
+
+kubectl apply -f /workspace/src/api/websocket-middleware/ingressRoute.yaml
