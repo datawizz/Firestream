4c32b93c77ffbafc88f1756e3a91395f97e9d6a7
Datawizz <36938650+datawizz@users.noreply.github.com>
2024-02-17 19:06:09 +0000

    [REF] Better build context

--- docker/docker_preinit.sh
+++ docker/docker_preinit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Determine OS platform
 OS_PLATFORM="$(uname -s)"
@@ -31,7 +32,6 @@ handle_linux() {
 # Function to handle Mac-specific operations
 handle_mac() {
     # macOS Docker desktop manages group permissions differently.
-    # Placeholder for macOS specific logic if needed.
     echo "Handling macOS specifics. Docker GID management not required."
     
     # Get the Machine ID for macOS
@@ -53,15 +53,14 @@ if [ -z "$HOST_MACHINE_ID" ]; then
     exit 1
 fi
 
-# Get the IP address of the host (used for end-to-end integration testing)
-# This command works on both Linux and macOS
+# Get the IP address of the host
 export HOST_IP=$(hostname -I 2>/dev/null || ipconfig getifaddr en0)
 if [ -z "$HOST_IP" ]; then
     echo "Failed to get Host IP."
     exit 1
 fi
 
-# Get the logged in user ID and Group ID (compatible with both Linux and macOS)
+# Get the logged in user ID and Group ID
 export HOST_USER_ID=$(id -u)
 export HOST_GROUP_ID=$(id -g)
 if [ -z "$HOST_USER_ID" ] || [ -z "$HOST_GROUP_ID" ]; then
@@ -69,44 +68,71 @@ if [ -z "$HOST_USER_ID" ] || [ -z "$HOST_GROUP_ID" ]; then
     exit 1
 fi
 
-# Function to extract GPU vendor for Linux
+# Extract GPU vendor for Linux
 extract_linux_gpu_vendor() {
-    # Check for the presence of lspci command
     if command -v lspci &> /dev/null; then
-        # Use lspci to find the GPU and extract the vendor name
-        vendor=$(lspci | grep -E "VGA|3D" | sed -r 's/^.*: ([^:]*):.*$/\1/')
-        echo "$vendor"
+        # Simplify extraction logic to focus on identifying NVIDIA
+        if lspci | grep -E "VGA|3D" | grep -iq "nvidia"; then
+            echo "NVIDIA"
+        else
+            echo "Other"
+        fi
     else
         echo "lspci command not found, unable to extract GPU vendor."
         exit 1
     fi
 }
 
-# Function to extract GPU model for macOS
+# Extract GPU model for macOS
 extract_macos_gpu_vendor() {
-    # Use system_profiler to find the GPU and extract the model
-    model=$(system_profiler SPDisplaysDataType | awk -F: '/Chipset Model/ {print $2}' | head -1 | sed 's/^ *//')
-    echo "$model"
+    # Check for Metal API support, simplifying the assumption to Apple GPUs
+    if system_profiler SPDisplaysDataType | grep -iq "Metal"; then
+        echo "Apple Metal"
+    else
+        echo "Other"
+    fi
 }
 
-# Check OS and extract GPU information
+# Extract GPU information
 if [ "$os" = "Linux" ]; then
     HOST_GPU_VENDOR=$(extract_linux_gpu_vendor)
-    echo "GPU Vendor/Model: $HOST_GPU_VENDOR"
 elif [ "$os" = "Mac" ]; then
     HOST_GPU_VENDOR=$(extract_macos_gpu_vendor)
-    echo "GPU Vendor/Model: $HOST_GPU_VENDOR"
 fi
+echo "GPU Vendor: $HOST_GPU_VENDOR"
 
-# Check if GPU vendor/model was extracted
-if [ -n "$HOST_GPU_VENDOR" ]; then
+# Set GPU_STATUS based on vendor detection
+if [[ "$HOST_GPU_VENDOR" == "NVIDIA" || "$HOST_GPU_VENDOR" == "Apple Metal" ]]; then
     export HOST_GPU_STATUS="true"
 else
-    echo "Failed to get GPU vendor/model."
     export HOST_GPU_STATUS="false"
 fi
 
 HOST_USERNAME=$(whoami)
+
+# Function to set variable based on file existence
+set_env_variable() {
+  local example_file_path="../etc/.env.secrets.example"
+  local expected_file_path="../etc/.env.secrets"
+
+  # Check if the file exists
+  if [ -e "$expected_file_path" ]; then
+    # File exists, set the variable to the file path
+    ENV_SECRETS_PATH="$expected_file_path"
+  else
+    # File does not exist, use the example secrets
+    ENV_SECRETS_PATH="$example_file_path"
+  fi
+
+  # Export the variable for global use (optional)
+  export ENV_SECRETS_PATH
+
+  # For debugging or confirmation, you can display the variable value
+  echo "ENV_SECRETS_PATH is set to: $ENV_SECRETS_PATH"
+}
+
+# To use this function, simply call it
+set_env_variable
 
 # Create a temporary docker-compose file with environment variables and build arguments
 TEMP_COMPOSE_FILE="docker/docker-compose.temp.yml"
@@ -129,15 +155,16 @@ TEMP_COMPOSE_FILE="docker/docker-compose.temp.yml"
   echo "        HOST_GPU_STATUS: $HOST_GPU_STATUS"
   echo "        HOST_GPU_VENDOR: $HOST_GPU_VENDOR"
   echo "        HOST_USERNAME: $HOST_USERNAME"
+  echo "    env_file:"
+  echo "      - $ENV_SECRETS_PATH"
 } > $TEMP_COMPOSE_FILE
-
 
 # Define the base and override file paths
 BASE_FILE="docker/docker-compose.yml"
-OVERRIDE_FILE="docker/docker-compose.gpu_override.yml"
 
 # Combine Compose Files based on GPU_STATUS
-if [ "$GPU_STATUS" = "true" ]; then
+if [ "$HOST_GPU_VENDOR" = "NVIDIA" ]; then
+    OVERRIDE_FILE="docker/docker-compose.gpu_nvidia.yml"
     docker compose -f $BASE_FILE -f $OVERRIDE_FILE -f $TEMP_COMPOSE_FILE config > docker/docker-compose.devcontainer.yml
 else
     docker compose -f $BASE_FILE -f $TEMP_COMPOSE_FILE config > docker/docker-compose.devcontainer.yml
