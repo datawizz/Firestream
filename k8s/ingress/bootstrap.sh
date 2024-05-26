fa22102ba6be6ecabe5430f316ab37291157516c
Datawizz <36938650+datawizz@users.noreply.github.com>
2024-05-26 15:27:17 +0000

    [REF] Manage Merge

--- k8s/ingress/bootstrap.sh
+++ k8s/ingress/bootstrap.sh
@@ -9,3 +9,8 @@ helm upgrade --install ngrok-ingress-controller ngrok/kubernetes-ingress-control
   --set credentials.authtoken=$NGROK_AUTHTOKEN 
 
 # kubectl apply -f /workspace/k8s/ingress/ingress.ngrok.yaml
+
+# Apply the environment variables to the kustomization template, without writing to a file
+# export SUBDOMAIN="your-unique-subdomain"
+# export MAIN_DOMAIN="ngrok.app"
+# cat overlays/production/kustomization.template.yaml | envsubst | kustomize build - | kubectl apply -f -
